964b4b644fa1f2b894729f80586626fd9512a287
Caiquera <caiquerantc@gmail.com>
2020-07-17 17:16:52 -0300

    Adição do sql as depedências

--- src/main/java/com/gentlemans/toDo/controller/ToDoController.java
+++ src/main/java/com/gentlemans/toDo/controller/ToDoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/toDo")
+@RequestMapping("/app")
 public class ToDoController {
     @Autowired
     private ToDoRepository toDoRepository;
