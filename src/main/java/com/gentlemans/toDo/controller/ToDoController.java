a7a4394759d1d1953fe9ac891f206ae39f2b87fe
Caiquera <caiquerantc@gmail.com>
2020-07-17 14:57:38 -0300

    Criação do Controller

---
+++ src/main/java/com/gentlemans/toDo/controller/ToDoController.java
@@ -0,0 +1,10 @@
+package com.gentlemans.toDo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notes")
+public class ToDoController {
+    
+}
