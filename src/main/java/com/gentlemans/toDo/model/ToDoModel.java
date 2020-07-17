d60c3bba4b39c0ff52f4cc80bdb36ca8fda0a842
Caiquera <caiquerantc@gmail.com>
2020-07-17 14:33:38 -0300

    Configuração application propeties

---
+++ src/main/java/com/gentlemans/toDo/model/ToDoModel.java
@@ -0,0 +1,32 @@
+package com.gentlemans.toDo.model;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Table
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ToDoModel {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "created_at", nullable = false)
+    @CreatedDate
+    private Date createdAt;
+}
