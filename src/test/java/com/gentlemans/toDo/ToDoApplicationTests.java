f5dbc03f391c4fb73b18d73acdcc359a2e9821d1
Caiquera <caiquerantc@gmail.com>
2020-07-17 20:53:11 -0300

    Introdução dos testes com JUnit

--- src/test/java/com/gentlemans/toDo/ToDoApplicationTests.java
+++ src/test/java/com/gentlemans/toDo/ToDoApplicationTests.java
@@ -1,13 +1,44 @@
 package com.gentlemans.toDo;
 
+import javafx.application.Application;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ToDoApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
+
+	private String getRootUrl(){
+		return "http://localhost:" + port;
+	}
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testGetAllUsers(){
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/notes", HttpMethod.GET, entity, String.class);
+		Assert.assertNotNull(response.getBody());
 	}
 
 }
