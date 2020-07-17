498c7f45d525a337858d8709f1f1336c04d1022d
Caiquera <caiquerantc@gmail.com>
2020-07-17 15:37:46 -0300

    Mapeamento do GET/id

--- src/main/java/com/gentlemans/toDo/controller/ToDoController.java
+++ src/main/java/com/gentlemans/toDo/controller/ToDoController.java
@@ -2,8 +2,11 @@ package com.gentlemans.toDo.controller;
 
 import com.gentlemans.toDo.model.ToDoModel;
 import com.gentlemans.toDo.repository.ToDoRepository;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +22,15 @@ public class ToDoController {
     public List<ToDoModel> getAllnotes(){
         return toDoRepository.findAll();
     }
-    
+
+    @GetMapping("/notes/{id}")
+    public ResponseEntity<ToDoModel> getNotesById(@PathVariable(value = "id")Integer noteId)
+        throws ResourceNotFoundException {
+        ToDoModel toDoModel =
+                toDoRepository
+                .findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException("Nota não encontrada no ::") + noteId);
+        return ResponseEntity.ok().body(toDoModel);
+    }
+
 }
