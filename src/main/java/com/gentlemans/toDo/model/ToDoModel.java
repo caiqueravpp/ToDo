e47100acba65ba00d7db522c31b5dbe204f662ed
Caiquera <caiquerantc@gmail.com>
2020-07-17 14:33:53 -0300

    Update ToDoModel.java

--- src/main/java/com/gentlemans/toDo/model/ToDoModel.java
+++ src/main/java/com/gentlemans/toDo/model/ToDoModel.java
@@ -3,7 +3,6 @@ package com.gentlemans.toDo.model;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
