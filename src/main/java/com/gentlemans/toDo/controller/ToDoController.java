fdd53f5e07bfe3214a950050f5d84203ee0198f9
Caiquera <caiquerantc@gmail.com>
2020-07-17 15:02:05 -0300

    Mapeamento do GET

--- src/main/java/com/gentlemans/toDo/controller/ToDoController.java
+++ src/main/java/com/gentlemans/toDo/controller/ToDoController.java
@@ -1,10 +1,23 @@
 package com.gentlemans.toDo.controller;
 
+import com.gentlemans.toDo.model.ToDoModel;
+import com.gentlemans.toDo.repository.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/notes")
+@RequestMapping("/toDo")
 public class ToDoController {
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    @GetMapping("/notes")
+    public List<ToDoModel> getAllnotes(){
+        return toDoRepository.findAll();
+    }
     
 }
