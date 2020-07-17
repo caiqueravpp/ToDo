63ba3b2fd11852a0abdcacd8d2db24bed7b2cdaf
Caiquera <caiquerantc@gmail.com>
2020-07-17 16:00:35 -0300

    Implementação do Post

--- src/main/java/com/gentlemans/toDo/controller/ToDoController.java
+++ src/main/java/com/gentlemans/toDo/controller/ToDoController.java
@@ -5,11 +5,9 @@ import com.gentlemans.toDo.repository.ToDoRepository;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -24,13 +22,18 @@ public class ToDoController {
     }
 
     @GetMapping("/notes/{id}")
-    public ResponseEntity<ToDoModel> getNotesById(@PathVariable(value = "id")Integer noteId)
+    public ResponseEntity<ToDoModel> getNoteById(@PathVariable(value = "id")Integer noteId)
         throws ResourceNotFoundException {
         ToDoModel toDoModel =
                 toDoRepository
                 .findById(noteId)
                 .orElseThrow(() -> new ResourceNotFoundException("Nota não encontrada no ::") + noteId);
         return ResponseEntity.ok().body(toDoModel);
+    }
+
+    @PostMapping("/notes")
+    public ToDoModel createNote(@Valid @RequestBody ToDoModel toDoModel) {
+        return toDoRepository.save(toDoModel);
     }
 
 }
