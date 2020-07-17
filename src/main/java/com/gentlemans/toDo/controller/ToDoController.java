395483634ea68541467964d98b8b60af810074f1
Caiquera <caiquerantc@gmail.com>
2020-07-17 16:52:46 -0300

    Implementação do Delete

--- src/main/java/com/gentlemans/toDo/controller/ToDoController.java
+++ src/main/java/com/gentlemans/toDo/controller/ToDoController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/toDo")
@@ -37,7 +39,7 @@ public class ToDoController {
         return toDoRepository.save(toDoModel);
     }
 
-    @PutMapping("/users/{id}")
+    @PutMapping("/notes/{id}")
     public ResponseEntity<ToDoModel> updateNote(
             @PathVariable(value = "id") Integer noteId, @Valid @RequestBody ToDoModel noteDetail)
         throws ResourceNotFoundException {
@@ -52,6 +54,19 @@ public class ToDoController {
         toDoModel.setUpdatedAt(new Date());
         final ToDoModel updateNote = toDoRepository.save(toDoModel);
         return ResponseEntity.ok(updateNote);
+    }
+
+    @DeleteMapping("/notes/{id}")
+    public Map<String, Boolean> deleteNote (@PathVariable(value = "id")Integer noteId) throws Exception {
+        ToDoModel toDoModel =
+                toDoRepository
+                .findById(noteId)
+                .orElseThrow(() -> new ResourceNotFoundException("Nota não encontrada no ::" + noteId));
+
+        toDoRepository.delete(toDoModel);
+        Map<String, Boolean> response = HashMap<>();
+        response.put("excluido", Boolean.TRUE);
+        return response;
     }
 
 }
